1328cd44edd0a5d5821d57037dd5a3798dab7a65
yawe13 <yawe13@gmail.com>
2013-07-01 14:53:56 +0200

    test

--- src/main/java/edu/fh/kanban/ui/view/BoardView.java
+++ src/main/java/edu/fh/kanban/ui/view/BoardView.java
@@ -4,19 +4,29 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 
 public class BoardView extends JPanel implements View{
 
+	private JTextField textfield = new JTextField();
+	
 	@Override
 	public JComponent getComponent() {
 		return this;
 	}
 	
 	public BoardView(){
-		
+		 FormLayout formLayout = new FormLayout("p,2dlu,p:grow");
+	     CellConstraints cc = new CellConstraints();
+	        
+	    DefaultFormBuilder builder = new DefaultFormBuilder(formLayout); 
 		JLabel label = new JLabel("Hallo");
-		this.add(label);
-		this.setVisible(true);
+		add(label);
+		add(textfield);
 	}
 
 	
