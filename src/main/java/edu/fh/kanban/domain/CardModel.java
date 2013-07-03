a8a193617addd9af283b6d188092145f883e86ed
ruby -v <yawe13@gmail.com>
2013-07-03 17:04:25 +0200

    Blocker und Karten-Model hinzugefügt

--- src/main/java/edu/fh/kanban/domain/CardModel.java
+++ src/main/java/edu/fh/kanban/domain/CardModel.java
@@ -1,5 +1,69 @@
 package edu.fh.kanban.domain;
 
-public class CardModel {
+import java.util.Date;
 
+public class CardModel {
+	private int id;
+	private int workload;
+	private int value;
+	private String description;
+	private Blocker blocker;
+	private Date created;
+	private Date started;
+	private Date done;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getWorkload() {
+		return workload;
+	}
+	public void setWorkload(int workload) {
+		this.workload = workload;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Blocker getBlocker() {
+		return blocker;
+	}
+	public void setBlocker(Blocker blocker) {
+		this.blocker = blocker;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Date getStarted() {
+		return started;
+	}
+	public void setStarted(Date started) {
+		this.started = started;
+	}
+	public Date getDone() {
+		return done;
+	}
+	public void setDone(Date done) {
+		this.done = done;
+	}
+	
+	
+	
+	 
+	
 }
