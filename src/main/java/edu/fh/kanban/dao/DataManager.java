c59f3ad550d64b02300ea370153b37b38b00d4ae
ruby -v <yawe13@gmail.com>
2013-07-03 16:51:59 +0200

    nochmal

---
+++ src/main/java/edu/fh/kanban/dao/DataManager.java
@@ -0,0 +1,28 @@
+package edu.fh.kanban.dao;
+
+import edu.fh.kanban.domain.XMLReader;
+
+public class DataManager {
+
+	public DataManager(){
+		
+	}
+	
+	public void readXML(){
+		XMLReader xmlreader = new XMLReader();
+		xmlreader.read();
+	}
+	
+	public void wirteXML(){
+		
+	}
+	
+	public void writeXLM(){
+		
+	}
+	
+	public void writePDF(){
+		
+	}
+	
+}
