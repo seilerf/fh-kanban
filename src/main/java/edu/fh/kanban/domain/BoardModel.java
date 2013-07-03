7e10076c2afd7fd04935ddbfb71f84177db796d4
Yannick <yawe13@gmail.com>
2013-07-03 14:08:35 +0200

    Test PropertyChangeListener

--- src/main/java/edu/fh/kanban/domain/BoardModel.java
+++ src/main/java/edu/fh/kanban/domain/BoardModel.java
@@ -6,18 +6,24 @@ import java.beans.PropertyChangeSupport;
 
 public class BoardModel {
 
-	private PropertyChangeSupport support ;
+	private PropertyChangeSupport changes ;
 	private String name;
 	private int id;
 	
 	public BoardModel(){
 		//merkt sich die Listener
-		support = new PropertyChangeSupport(this);
+		changes = new PropertyChangeSupport(this);
 
 	}
 	
-	 public void addPropertyChangeListener(PropertyChangeListener listener) {
-		 support.addPropertyChangeListener(listener);
+	public void setName(){
+		String oldName = this.name;
+		this.name = name;
+		changes.firePropertyChange("name", oldName, name);
+	}
+	
+	 public void addPropertyChangeListener(PropertyChangeListener l) {
+		 changes.addPropertyChangeListener(l);
 	 }
 
 
