0dc2c2edfa294a213a7052c4337033befba2a979
Maximilian Ullmann <max.ullmann@gmx.de>
2013-07-06 11:56:27 +0200

    CardView Board Skalierung

--- src/main/java/edu/fh/kanban/ui/view/CardViewBoard.java
+++ src/main/java/edu/fh/kanban/ui/view/CardViewBoard.java
@@ -19,31 +19,29 @@ import javax.swing.JTextPane;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 
 public class CardViewBoard extends JPanel implements View {
 	private JTextField idTextField;
 	private JTextField aufwandTextField;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Create the panel.
 	 */
 	public CardViewBoard() {
 		setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Karte", TitledBorder.CENTER, TitledBorder.TOP, null, Color.BLACK));
-		setLayout(new FormLayout(new ColumnSpec[] {
+		FormLayout formLayout = new FormLayout(new ColumnSpec[] {
 				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("15dlu"),
+				ColumnSpec.decode("45dlu"),
 				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("55dlu"),
+				ColumnSpec.decode("45dlu"),
 				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
+				ColumnSpec.decode("40dlu"),
 				FormFactory.RELATED_GAP_COLSPEC,
 				ColumnSpec.decode("max(60dlu;default)"),
 				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("max(55dlu;default)"),
-				FormFactory.RELATED_GAP_COLSPEC,
 				ColumnSpec.decode("max(40dlu;default)"),
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
 				FormFactory.RELATED_GAP_COLSPEC,
 				FormFactory.DEFAULT_COLSPEC,},
 			new RowSpec[] {
@@ -54,7 +52,7 @@ public class CardViewBoard extends JPanel implements View {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				RowSpec.decode("max(14dlu;default)"),
 				FormFactory.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("max(14dlu;default)"),
+				RowSpec.decode("max(25dlu;default)"),
 				FormFactory.RELATED_GAP_ROWSPEC,
 				RowSpec.decode("max(14dlu;default)"),
 				FormFactory.RELATED_GAP_ROWSPEC,
@@ -62,14 +60,16 @@ public class CardViewBoard extends JPanel implements View {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				RowSpec.decode("max(14dlu;default)"),
 				FormFactory.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("max(14dlu;default)"),
+				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
 				RowSpec.decode("max(14dlu;default)"),
 				FormFactory.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("max(14dlu;default)"),}));
-		
+				RowSpec.decode("max(14dlu;default)"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("max(14dlu;default)"),});
+		setLayout(formLayout);
 		JLabel idLabel = DefaultComponentFactory.getInstance().createLabel("ID:");
-		idLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		idLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		add(idLabel, "2, 2, right, fill");
 		
 		idTextField = new JTextField();
@@ -77,44 +77,48 @@ public class CardViewBoard extends JPanel implements View {
 		idTextField.setColumns(1);
 		
 		JLabel aufwandLabel = DefaultComponentFactory.getInstance().createLabel("Aufwand:");
-		aufwandLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(aufwandLabel, "8, 2, right, fill");
+		aufwandLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		add(aufwandLabel, "2, 4, right, fill");
 		
 		aufwandTextField = new JTextField();
-		add(aufwandTextField, "10, 2, fill, fill");
+		add(aufwandTextField, "4, 4, fill, fill");
 		aufwandTextField.setColumns(1);
 		
+		JToggleButton blockerToggleButton = new JToggleButton("Blocker");
+		blockerToggleButton.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		blockerToggleButton.setForeground(Color.RED);
+		add(blockerToggleButton, "6, 4, 1, 3");
+		
 		JLabel wertLabel = DefaultComponentFactory.getInstance().createLabel("Wert:");
-		wertLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(wertLabel, "8, 6, right, fill");
+		wertLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		add(wertLabel, "2, 6, right, fill");
 		
 		JComboBox wertComboBox = new JComboBox();
+		wertComboBox.setFont(new Font("Tahoma", Font.PLAIN, 8));
 		wertComboBox.setModel(new DefaultComboBoxModel(new String[] {"Wähle aus:", "1: Blau", "2: Orange", "3: Rot", "4: Grün"}));
-		add(wertComboBox, "10, 6, fill, default");
-		
-		JToggleButton blockerToggleButton = new JToggleButton("Blocker");
-		blockerToggleButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		blockerToggleButton.setForeground(Color.RED);
-		add(blockerToggleButton, "3, 8, 3, 5");
+		add(wertComboBox, "4, 6, fill, default");
 		
 		JLabel beschreibungLabel = DefaultComponentFactory.getInstance().createLabel("Beschreibung:");
-		beschreibungLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(beschreibungLabel, "8, 10, right, top");
+		beschreibungLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		add(beschreibungLabel, "2, 8, right, top");
 		
 		JTextPane beschreibungTextPane = new JTextPane();
-		add(beschreibungTextPane, "10, 10, 3, 5, fill, fill");
+		add(beschreibungTextPane, "4, 8, 3, 2, fill, fill");
 		
 		JRadioButton rdbtnCreated = new JRadioButton("Created");
-		rdbtnCreated.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(rdbtnCreated, "8, 18, right, fill");
+		buttonGroup.add(rdbtnCreated);
+		rdbtnCreated.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		add(rdbtnCreated, "2, 10, center, fill");
 		
 		JRadioButton rdbtnStarted = new JRadioButton("Started");
-		rdbtnStarted.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(rdbtnStarted, "10, 18, right, fill");
+		buttonGroup.add(rdbtnStarted);
+		rdbtnStarted.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		add(rdbtnStarted, "4, 10, center, fill");
 		
 		JRadioButton rdbtnDone = new JRadioButton("Done");
-		rdbtnDone.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(rdbtnDone, "12, 18, right, fill");
+		buttonGroup.add(rdbtnDone);
+		rdbtnDone.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		add(rdbtnDone, "6, 10, center, fill");
 
 	}
 
