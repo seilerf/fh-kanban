4d65f043eb3bc758019f2a57db4d8aa6481019a5
Florian Seiler <f.seiler@ostfalia.de>
2013-08-12 00:37:25 +0200

    Revert "PrefTest aktualisiert"
    This reverts commit 0a6861a01ef28d3741a24bb9fc0009f9f2cb8355.

--- src/test/java/edu/fh/kanban/ui/PreferencesTest.java
+++ src/test/java/edu/fh/kanban/ui/PreferencesTest.java
@@ -1,4 +1,4 @@
-﻿package edu.fh.kanban.ui;
+package edu.fh.kanban.ui;
 
 import java.awt.Color;
 import java.awt.GridBagConstraints;
@@ -10,7 +10,6 @@ import java.awt.event.ItemListener;
 import java.io.File;
 import java.text.ParseException;
 import java.util.Iterator;
-import java.util.LinkedList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -24,24 +23,17 @@ import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
-import edu.fh.kanban.dao.BoardDAO;
-import edu.fh.kanban.dao.XML.XMLUtil;
-import edu.fh.kanban.domain.Backlog;
 import edu.fh.kanban.domain.Board;
 import edu.fh.kanban.domain.Card;
 import edu.fh.kanban.domain.ColorBox;
-import edu.fh.kanban.domain.Column;
-import edu.fh.kanban.ui.controller.BacklogController;
-import edu.fh.kanban.ui.controller.BoardController;
-import edu.fh.kanban.ui.view.BacklogView;
-import edu.fh.kanban.ui.view.BoardView;
 
 public class PreferencesTest {
-	
+	/*
 	public static void main(String[] args) throws ParseException, InterruptedException{
 		
+final DataManager dm = new DataManager();
 		
-		
+		dm.openFile(new File("board.xml"));
 		
 		//Board board = dm.getBoard();
 		
@@ -75,14 +67,7 @@ public class PreferencesTest {
             public void actionPerformed(ActionEvent e) {
                 
             	String boardname = name.getText();
-            	//board.setName(boardname);
-            	try {
-					ändereBoardNamen(e,boardname);
-				} catch (NullPointerException | ParseException
-						| InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+            	ändereBoardNamen(e,boardname);
             }
         }); 
         
@@ -133,31 +118,17 @@ public class PreferencesTest {
 	 * setted den Neuen Boardnamen und gibt vorher den alten und dann den neuen Boardnamen als Beweis zurück
 	 * @param e
 	 * @param name
-	 * @throws InterruptedException 
-	 * @throws ParseException 
-	 * @throws NullPointerException 
 	 */
-	
-	public static void ändereBoardNamen(ActionEvent e, String name) throws NullPointerException, ParseException, InterruptedException {
+	/*
+	public static void ändereBoardNamen(ActionEvent e, String name) {
 		
-		BoardDAO boardDAO;
-		final Board board;
-		BoardView boardView;
-		
-		boardDAO = XMLUtil.getXmlFactory().getBoardDAO();
-		board = boardDAO.findBoard();
-
-		
-		BoardController boardController;
-		Backlog backlog;
-		BacklogView backlogView;
-		BacklogController backlogController;
-		
-		//boardController = new BoardController();
-		//boardController.addModel(board);
+        DataManager dm= new DataManager();
+        Board   board=dm.getBoard();
+        
+      
         System.out.println(board.getName());
         
        board.setName(name);
        System.out.println(board.getName());
-	}
+	}*/
 }
